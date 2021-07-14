132f57a84609c9af32dfaf035a29a60a69c55f20
AlexZe-NET <85558162+AlexZe-NET@users.noreply.github.com>
2021-07-14 10:28:01 +0200

    ClassLibrary for API consumer project
    All classes needed for consuming APIs put in a separate classLibrary

---
+++ SunProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SunProcessor
+    {
+        public static async Task<SunModel> LoadSunInformation()
+        {
+            string url = "https://api.sunrise-sunset.org/json?lat=44.871716&lng=20.325249&date=today";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    SunResultModel result = await response.Content.ReadAsAsync<SunResultModel>();
+
+                    return result.Results;
+                }
+
+                throw new Exception(response.ReasonPhrase);
+            }
+
+        }
+    }
+}
